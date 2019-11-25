80fed860916697786b3169e0cd136a2f8c769150
SADmammoth <lo.ma200018@gmail.com>
2019-11-25 15:47:59 +0300

    feat: blog page

---
+++ wp-content/themes/palichka/archive-post.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template name: Блог
+ * Template Post Type: page
+ * 
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package palichka
+ */
+
+get_header();
+require get_template_directory() . '/template-parts/archive-template.php'
+?>
+
+<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+     <h1 class='h1-small background wide-toleft'>Новости и блог</h1>
+      <section class='cards vertical-flex-block wide-toleft' style='min-height: 49vh'>
+      <?php $archive = get_the_archive_template('post');
+          echo($archive['cards']);
+      ?>
+      </section>
+      <?php
+          echo($archive['sidebar']); 
+      ?>
+      </main>
+</div>
+<?php
+get_footer();
