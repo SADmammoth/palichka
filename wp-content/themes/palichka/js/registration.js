9f7efa4edc373dc942d993f4356d60e47b1d493f
SADmammoth <lo.ma200018@gmail.com>
2019-11-27 01:41:08 +0300

    fix (register): incorrect form validation

--- wp-content/themes/palichka/js/registration.js
+++ wp-content/themes/palichka/js/registration.js
@@ -1,47 +1,93 @@
-function check_username(input) {
+function check_submit(button) {
+  event.preventDefault();
+  alert(me.form.email_validated);
+  if (button.from.validate()) {
+    if (
+      button.form.login_validated &&
+      button.form.email_validated &&
+      button.form.password_validated
+    ) {
+      button.form.submit();
+    }
+  } else {
+    user_message("Заполните все поля");
+  }
+}
+
+async function check_validity(me) {
+  me.form.login_validated = await check_username(
+    me.form.elements["login"],
+    !me.form.email_validated
+  );
+  me.form.email_validated = await check_email(me.form.elements["email"], !me.form.login_validated);
+}
+
+async function check_username(input, hide_message = true) {
   let username = input.value;
-  fetch(input.form.action, {
+  let flag = true;
+  let response = await fetch(input.form.action, {
     method: "POST",
     body: JSON.stringify({ username: username }),
     headers: {
       "Content-Type": "application/json"
     }
-  })
-    .then(response => {
-      response
-        .json()
-        .then(data =>
-          data.userfound ? user_message("Имя пользователя уже занято") : hide_user_message()
-        );
+  });
+  response
+    .json()
+    .then(data => {
+      if (data.userfound) {
+        user_message("Имя пользователя уже занято");
+        flag = false;
+      } else {
+        if (hide_message) {
+          hide_user_message();
+        }
+      }
     })
-    .catch(message => user_message(message));
+    .catch(message => {
+      user_message(message);
+      flag = false;
+    });
+  return flag;
 }
 
-function check_email(input) {
+async function check_email(input, hide_message = true) {
   let email = input.value;
-  fetch(input.form.action, {
+  input.form.email_validated = false;
+  let flag = false;
+  let response = await fetch(input.form.action, {
     method: "POST",
     body: JSON.stringify({ email: email }),
     headers: {
       "Content-Type": "application/json"
     }
-  })
-    .then(response => {
-      response
-        .json()
-        .then(data =>
-          data.userfound ? user_message("Email уже зарегистрирован") : hide_user_message()
-        );
+  });
+  response
+    .json()
+    .then(data => {
+      if (data.userfound) {
+        user_message("Email уже зарегистрирован");
+        flag = false;
+      } else {
+        if (hide_message) {
+          hide_user_message();
+        }
+      }
     })
-    .catch(message => user_message(message));
+    .catch(message => {
+      user_message(message);
+      flag = false;
+    });
+  return flag;
 }
 
-function check_password() {
+function check_password(button) {
   event.preventDefault();
   let password = document.getElementById("password").value;
   if (password !== document.getElementById("repeat_password").value) {
     password_message("Пароли не совпадают");
-    return;
+    button.from.password_validated = false;
+    return false;
   }
   let password_reqexp = new RegExp(
     `^(?=([a-zA-Z0-9_]{8,}))(?=.*[0-9])${
@@ -60,9 +106,12 @@ function check_password() {
       <li>Содержит цифры и заглавные буквы</li>
       </ul>`
     );
-    return;
+    button.from.password_validated = false;
+    return false;
   }
   hide_password_message();
+  button.from.password_validated = true;
+  return true;
 }
 
 function user_message(text) {
--- wp-content/themes/palichka/page-register.php
+++ wp-content/themes/palichka/page-register.php
@@ -8,9 +8,8 @@ get_header();
       <form id='registration-form' action='<?php echo get_template_directory_uri().'/inc/users.php'?>' method='POST' class='insidelayout-fromleft'>
       <h2 style='margin-bottom: -5px; padding-top: 20px;'>Регистрация</h2>
       <fieldset class='form-head' style='position: relative;'>
-
-        <input class='form-input' id='name' name='login' type='text' placeholder='Логин' onchange='check_username(this)' require/>
-        <input class='form-input' name='email' type='email' placeholder='Почта'  onchange='check_email(this)' require/>
+        <input class='form-input' id='name' name='login' type='text' placeholder='Логин' onchange='check_validity(this)' required/>
+        <input class='form-input' name='email' type='email' placeholder='Почта'  onchange='check_validity(this)' required/>
         <div id='user_error' class='hint error-message hidden' style='position: absolute; left: calc(100% + 10px); width: 80%; top: 10px;'>
         <i class='fas fa-times-circle' style='float: left; padding-right: 15px; clear: none;'></i>
         <span class='message' style='float: left; clear: none; width: 90%;'></span>
@@ -18,16 +17,14 @@ get_header();
         </fieldset>
         <fieldset class='form-body' style='position: relative;'>
 
-        <input class='form-input' name='password' id='password'type='password' placeholder='Пароль' require/>
-        <input class='form-input' id='repeat_password' type='password' placeholder='Повторите пароль' require/>
+        <input class='form-input' name='password' id='password'type='password' placeholder='Пароль' required/>
+        <input class='form-input' id='repeat_password' type='password' placeholder='Повторите пароль' required/>
         <div id='password_error' class='hint error-message hidden' style='position: absolute; left: calc(100% + 10px); width: 80%; top: 10px;'>
         <i class='fas fa-times-circle' style='float: left; padding-right: 15px; clear: none;'></i>
         <span class='message' style='float: left; clear: none; width: 90%;'></span>
         </div>
         </fieldset>
-       
-        <button class='button' onclick='check_password(this);'>Зарегистрироваться</button>
-        
+        <button class='button' onclick='check_submit(this);'>Зарегистрироваться</button>
       </form>
     
       </section>
